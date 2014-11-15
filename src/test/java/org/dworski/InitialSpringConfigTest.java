a71dfb83af03172d2d6d2ff372524b3e9044e2a0
lakiluk <lukasdw@gmail.com>
2014-11-15 18:51:26 +0100

    spring - property placeholder

--- src/test/java/org/dworski/InitialSpringConfigTest.java
+++ src/test/java/org/dworski/InitialSpringConfigTest.java
@@ -3,6 +3,7 @@ package org.dworski;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
@@ -17,11 +18,17 @@ public class InitialSpringConfigTest {
     @Autowired
     private Car car;
 
+    @Value("${car.make}")
+    private String carMake;
+
+    @Value("${car.productionYear}")
+    private String carProductionYear;
+
     @Test
     public void shouldGetCarObjectFromContext() {
         assertNotNull(car);
-        assertEquals("SEAT", car.getMake());
-        assertEquals(2010, car.getProductionYear());
+        assertEquals(carMake, car.getMake());
+        assertEquals(carProductionYear, String.valueOf(car.getProductionYear()));
     }
 
 }
