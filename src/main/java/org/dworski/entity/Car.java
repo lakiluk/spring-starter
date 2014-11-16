bdaad2dab88b7a73c7c444db08aeb95392042a5c
lakiluk <lukasdw@gmail.com>
2014-11-16 15:26:59 +0100

    spring - hibernate annotation config

--- src/main/java/org/dworski/entity/Car.java
+++ src/main/java/org/dworski/entity/Car.java
@@ -1,6 +1,5 @@
 package org.dworski.entity;
 
-import javax.annotation.Generated;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
